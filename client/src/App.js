6128eee6748787bba0a729f150e1f2e7223eac0c
Your Name <akshayjaiswal09052000@gmail.com>
2022-10-06 18:08:55 +0530

    Banner Completed

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/header/Navbar";
+import Maincomp from "./components/home/Maincomp";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Maincomp />
+    </>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+const port = 8005
+
+app.listen(process.env.PORT||port, ()=>{
+    console.log(`server is running on: ${port}`)
+})
